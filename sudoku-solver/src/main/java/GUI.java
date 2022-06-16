3fa0dcf689f50cc3aab32163b34f2a3ce2b81bc3
Kush <97338412+KushBharakhada@users.noreply.github.com>
2022-06-16 22:04:35 +0100

    User input validation and error handling.

--- sudoku-solver/src/main/java/GUI.java
+++ sudoku-solver/src/main/java/GUI.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GUI extends JFrame implements ActionListener {
 
@@ -16,6 +18,7 @@ public class GUI extends JFrame implements ActionListener {
     private JMenuItem solve;
     private int[][] inputValues;
     private int[][] outputValues;
+    private int[][] localGridTopLeftCoords = {{0,0}, {0,6}, {3,3}, {6,0}, {6,6}};
 
     public GUI() {
 
@@ -42,15 +45,16 @@ public class GUI extends JFrame implements ActionListener {
             for (int column = 0; column < GRID_LENGTH; column++) {
                 textBoxGrid[row][column] = new JTextField();
                 textBoxGrid[row][column].setSize(BOX_SIZE, BOX_SIZE);
-                textBoxGrid[row][column].setFont(new Font("Consoles", Font.PLAIN, 35));
+                textBoxGrid[row][column].setFont(new Font("Arial", Font.BOLD, 35));
+                textBoxGrid[row][column].setHorizontalAlignment(JTextField.CENTER);
                 this.add(textBoxGrid[row][column]);
             }
         }
 
         // Painting some 3x3 squares for visual clarity
-        int[][] paintLocalGrids = {{0,0}, {0,6}, {3,3}, {6,0}, {6,6}};
-        for (int i = 0; i < paintLocalGrids.length; i++) {
-            paintLocalSquares(paintLocalGrids[i][0], paintLocalGrids[i][1]);
+
+        for (int i = 0; i < localGridTopLeftCoords.length; i++) {
+            paintLocalSquares(localGridTopLeftCoords[i][0], localGridTopLeftCoords[i][1]);
         }
 
         this.setSize(FRAME_SIZE, FRAME_SIZE);
@@ -61,31 +65,98 @@ public class GUI extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
         if (e.getSource() == solve) {
+
+            boolean isValidIntegers = true;
+
+
             // Store user input values in a 2D integer grid
             // If spaces are left empty, store 0 in place
             for (int row = 0; row < GRID_LENGTH; row++) {
                 for (int column = 0; column < GRID_LENGTH; column++) {
                     if (textBoxGrid[row][column].getText().isEmpty())
                         inputValues[row][column] = 0;
+                    else if (textBoxGrid[row][column].getText().length() == 1) {
+                        try {
+                            inputValues[row][column] = Integer.valueOf(textBoxGrid[row][column].getText());
+                        }
+                        catch (NumberFormatException ex) {
+                            isValidIntegers = false;
+                        }
+                    }
                     else
                         inputValues[row][column] = Integer.valueOf(textBoxGrid[row][column].getText());
                 }
             }
-
-            outputBoard();
-
-            for (int[] y : outputValues) {
-                for (int x : y)
-                    System.out.print(x);
-                System.out.println();
-            }
-
-
+            if (isBoardValidInitially() && isValidIntegers)
+                outputBoard();
+            else
+                boardReset();
         }
     }
 
+    public boolean isBoardValidInitially() {
+        int[] listCheck = new int[9];
+
+        // Check if rows contain a duplicate
+        for (int i = 0; i < GRID_LENGTH; i++) {
+=            if (checkDuplicateInList(inputValues[i]))
+                return false;
+        }
+
+        // Check if columns contain a duplicate
+        for (int i = 0; i < GRID_LENGTH; i++) {
+            for (int j = 0; j < GRID_LENGTH; j++)
+                listCheck[j] = inputValues[j][i];
+            if (checkDuplicateInList(listCheck))
+                return false;
+        }
+
+        // Check local box contains a duplicate
+        int counter = 0;
+        for (int i = 0; i < localGridTopLeftCoords.length; i++) {
+            int row = localGridTopLeftCoords[i][0];
+            System.out.println(counter);
+            if (counter == GRID_LENGTH)
+                counter = 0;
+            for (int y = row; y < LOCAL_BOX_LENGTH + row; y++) {
+                int column = localGridTopLeftCoords[i][1];
+                for (int x = column; x < LOCAL_BOX_LENGTH + column; x++) {
+                    listCheck[counter] = inputValues[y][x];
+                    counter++;
+                }
+            }
+            if (checkDuplicateInList(listCheck))
+                return false;
+        }
+        return true;
+    }
+
+    public void boardReset() {
+        // Sets all values in the grid to 0
+        for (int row = 0; row < GRID_LENGTH; row++) {
+            for (int column = 0; column < GRID_LENGTH; column++) {
+                textBoxGrid[row][column].setText(String.valueOf(0));
+            }
+        }
+    }
+
+    public boolean checkDuplicateInList(int[] list) {
+        Set<Integer> duplicateTempList = new HashSet<Integer>();
+        for (int i : list) {
+            if (duplicateTempList.contains(i) && i != 0)
+                return true;
+            duplicateTempList.add(i);
+        }
+        return false;
+    }
+    
     public void outputBoard() {
         outputValues = Solver.solvedBoard(inputValues);
+        for (int row = 0; row < GRID_LENGTH; row++) {
+            for (int column = 0; column < GRID_LENGTH; column++) {
+                textBoxGrid[row][column].setText(String.valueOf(outputValues[row][column]));
+            }
+        }
     }
 
     public void paintLocalSquares(int row, int column) {
